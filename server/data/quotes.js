bac522907be22034099bbd046554985a2e1184e3
Azamat-div <73755645+Azamat-div@users.noreply.github.com>
2025-07-22 00:55:13 +0500

    Creating own quotes API Node.js Express server with resource /quotes/random-single

---
+++ server/data/quotes.js
@@ -0,0 +1,19 @@
+const quotes = [
+  {
+    id: 'f9a3c1d7',
+    text: 'Believe you can and you’re halfway there',
+    author: 'Theodore Roosevelt',
+  },
+  {
+    id: 'e4b7f2a9',
+    text: 'What we think, we become',
+    author: 'Buddha',
+  },
+  {
+    id: 'c6d8e3b1',
+    text: 'Happiness depends upon ourselves',
+    author: 'Aristotle',
+  },
+];
+
+module.exports = quotes;
